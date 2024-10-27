82ffb622419809969e70db29ad649c2817090314
cihan base <cihanbas07@gmail.com>
2024-10-27 09:32:23 +0300

    http get

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, View, Button, ScrollView, Text } from "react-native";
 
 export default function App() {
+  const [listData, setListData] = useState([]);
+  const getUsers = () => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((liste) => {
+        setListData(liste);
+      });
+  };
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
+      <Button title="GET Data" onPress={() => getUsers()}></Button>
+      <ScrollView>
+        <Text>{listData.length}</Text>
+      </ScrollView>
     </View>
   );
 }
@@ -13,5 +26,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    marginTop: 50,
+    justifyContent: "center",
   },
 });
